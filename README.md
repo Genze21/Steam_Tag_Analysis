a10868d9b7fbfd2d57bd896389eac2897b35b278
Genze21 <leonli1999@live.nl>
2022-05-22 19:25:26 +0200

    Data statistics

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# bklas
- 
+# Bklas
+All scripts expect the relevant data files in the data directory. A caching feature is used for ease of use. To use this the first time running the program set the `makeData` variable to `True`. This will create a csv files(cache). Next time when running the same program these files can be used as a cache can by setting the `makeData` to `False`. 
+
+# Installation
+All scripts should be run with a modern version of Python 3 (3.9 or higher is prefered). \
+Some scripts require extra dependencies. Make sure to install all required ones from the requirements.txt. 
+```
+pip3 install -r requirements.txt
+```
--- SteamAPIToReadable.py
+++ SteamAPIToReadable.py
@@ -1,8 +1,10 @@
+from pydoc import describe
 import requests
 import json
 import pandas as pd
 import os
 
+# dictionary counting inlcluded genres
 genresDict = {
 'openWorld' : 'http://steamspy.com/api.php?request=tag&tag=Open+World',
 'earlyAcces' : 'http://steamspy.com/api.php?request=tag&tag=Early+Access',
@@ -10,19 +12,42 @@ genresDict = {
 'action' :'http://steamspy.com/api.php?request=tag&tag=Action'}
 
 
-for i in genresDict:
-	response = requests.get(genresDict[i]).json()
+def main():
+	makeData = False
+	dataFolder = './data/'
 
-	# creat temporary json file for pandas
-	with open('tmp.json', 'w') as json_file:
-		json_file.write(json.dumps(response, sort_keys=True, indent = 4))
+	if makeData:
+		for i in genresDict:
+			response = requests.get(genresDict[i]).json()
 
-	# convert json to csv
-	df_json = pd.read_json('tmp.json')
-	# delete temporary file
-	
-	filename = 	'./data/' + i + '.csv'
-	df_json.to_csv(filename)
+			# creat temporary json file for pandas
+			with open('tmp.json', 'w') as json_file:
+				json_file.write(json.dumps(response, sort_keys=True, indent = 4))
 
-# remove temporary file
-os.remove('tmp.json')
+			# convert json to csv
+			df_json = pd.read_json('tmp.json')
+			# flip rows and columns
+			df_json = df_json.transpose()
+			
+			filename = 	'./data/' + i + '.csv'
+			df_json.to_csv(filename)
+
+		# remove temporary file
+		os.remove('tmp.json')
+
+	# print statistics for each file
+	directory = os.fsencode(dataFolder)
+	for file in os.listdir(directory):
+		fileName = os.fsdecode(file)
+		if fileName.endswith('.csv'):
+			df = pd.read_csv(dataFolder + fileName)
+			print('--------------------------')
+			print('Stats for ' + fileName)
+			print('--------------------------')
+			print(df[['average_forever','negative','positive']].describe())
+			print(f"Highest average_forever \n {df[df.average_forever == df.average_forever.max()]}")
+			print(f"Highest negative \n {df[df.negative == df.negative.max()]}")
+			print(f"Highest positive \n {df[df.positive == df.positive.max()]}")
+
+if __name__ == '__main__':
+	main()
